c2f5761f66377c76356734ab43a1b30c37588075
FrwSoftware <frwsoftware@gmail.com>
2017-09-05 17:19:33 +0300

    Add comments header to top of files

--- FrwSimpleWinCRUDDemo/Model/Chinook/Playlist.cs
+++ FrwSimpleWinCRUDDemo/Model/Chinook/Playlist.cs
@@ -1,4 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿/**********************************************************************************
+ *   FrwSimpleWinCRUD   https://github.com/frwsoftware/FrwSimpleWinCRUD
+ *   The Open-Source Library for most quick  WinForm CRUD application creation
+ *   MIT License Copyright (c) 2016 FrwSoftware
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ **********************************************************************************/
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using FrwSoftware;
 using System.Collections.Generic;
--- FrwSimpleWinCRUDTemplate/AboutDialog.cs
+++ FrwSimpleWinCRUDTemplate/AboutDialog.cs
@@ -1,13 +1,19 @@
+/**********************************************************************************
+ *   FrwSimpleWinCRUD   https://github.com/frwsoftware/FrwSimpleWinCRUD
+ *   The Open-Source Library for most quick  WinForm CRUD application creation
+ *   MIT License Copyright (c) 2016 FrwSoftware
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ **********************************************************************************/
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
-using System.Reflection;
 using WeifenLuo.WinFormsUI.Docking;
-using FrwSoftware;
 
 namespace FrwSoftware
 {
